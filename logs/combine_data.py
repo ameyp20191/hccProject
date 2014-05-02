2790fcbff62ebc41dc8ff3b2b47d369dfd64c774
S <sldas@outlook.com>
2014-05-01 21:30:36 -0500

    Add some logs and a tool to combine them.

---
+++ logs/combine_data.py
@@ -0,0 +1,85 @@
+"""Combine individual logs into one big JSON file."""
+
+import json
+
+
+def transform_log(fname, extra):
+    """Transform a log by adding some extra data.
+
+    The log should be parseable as a dictionary. Extra data will be added to
+    this dictionary as key/value pairs.
+
+    Args:
+      fname: Path of the log file.
+      extra: Extra information to add, dictionary.
+
+    Returns:
+      Parsed log with extra data added.
+
+    """
+    with open(fname, 'r') as f:
+        data = json.load(f)
+        data.update(extra)
+
+    return data
+
+
+def combine_logs(log_info):
+    """Combine logs into one object.
+
+    Args:
+      log_info: List of (fname, extra) tuples for each log file.
+
+    Returns:
+      List of parsed logs.
+
+    """
+    print "Combining logs: "
+    print log_info
+
+    combined = []
+    for fname, extra in log_info:
+        combined.append(transform_log(fname, extra))
+
+    return combined
+
+
+def main():
+    # Common keys
+    name, state, when, length = 'name', 'state', 'when', 'length'
+
+    # Common values
+    on, off = 'ON', 'OFF'
+    first, second = 1, 2
+    min_30 = 30
+
+    # Add log info here
+    sr_logs = [('sw_on_off/1_on.json',
+                {name: 'SR', state: on, when: first, length: min_30}),
+               ('sw_on_off/2_off.json',
+                {name: 'SR', state: off, when: second, length: min_30})]
+
+    zy_logs = [('zy_on_off/study_on.json',
+                {name: 'ZY', state: on, when: first, length: min_30}),
+               ('zy_on_off/study_off.json',
+                {name: 'ZY', state: off, when: second, length: min_30})]
+
+    all_logs_mixed = [sr_logs, zy_logs]
+
+    # Make proper log_info list
+    all_logs = []
+    for log_info in all_logs_mixed:
+        if isinstance(log_info, list):
+            all_logs.extend(log_info)
+        else:
+            all_logs.append(log_info)
+
+    combined = combine_logs(all_logs)
+    out = '/tmp/combined.json'
+    with open(out, 'w') as f:
+        json.dump(combined, f)
+    print "Combined logs saved to %s." % (out)
+
+
+if __name__ == "__main__":
+    main()
